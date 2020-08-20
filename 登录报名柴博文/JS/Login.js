b3736ca120f3d3a27318d31c77cdd27835a8feeb
lovebaihezi <x18739168862@gmail.com>
2020-08-20 21:03:26 +0800

    30% complete

--- 登录报名柴博文/JS/Login.js
+++ 登录报名柴博文/JS/Login.js
@@ -15,6 +15,7 @@ postChoose.prototype = {
             that.post = "frontEndDevelopment";
         });
         this.backStageManagement.addEventListener("click", function() {
+            // that.fillScreen(that.backStageManagement);
             that.post = "backStageManagement";
         });
     },
@@ -22,5 +23,77 @@ postChoose.prototype = {
         return this.post;
     }
 }
+
+function Submit() {
+    this.account = "";
+    this.password = "";
+    this.telePhoneNumber = "";
+    this.classInformation = "";
+    this.signInStatus = false;
+    this.accountLogInStatus = false;
+    this.allInformation = undefined;
+}
+
+//正则检查
+//点击上传
+//数据检查
+
+Submit.prototype = {
+    check: function(NeedCheck, Rules) {
+        return Rules.test(NeedCheck);
+    },
+    statusAccount: function() {
+        return ["account", "password"];
+    },
+    statusInformation: function() {
+        return ["telePhoneNumber", "classInformation"];
+    },
+    statusAccountCheck: function() {
+        return ["account", /^\d{6}$/g];
+    },
+    statusInformationCheck: function() {
+        return ["telePhoneNumber", /^\d{13}$/g];
+    },
+    alive: function() {
+        var This = this;
+        var StatusString = "";
+        document.getElementById("clickThenSubmit").onclick = function() {
+            if (This.accountLogInStatus) {
+                This.getInformation(This["statusInformation"]()[0], This["statusInformation"]()[1]);
+            } else {
+                This.getInformation(This["statusAccount"]()[0], This["statusAccount"]()[1]);
+            }
+            This.setInformation();
+            This.clickAnimation();
+        }
+    },
+    clickAnimation: function() {
+        if (this.loginStatus) {
+
+        }
+    }, //数据OK才开始动画,并且要跳转到下一个,status都要改
+    getInformation: function(information1, information2) {
+        this[information1] = document.getElementById("information1").value;
+        this[information2] = document.getElementById("information2").value;
+    },
+    setInformation: function() {
+        this.allInformation = {
+            "account": this.account,
+            "password": this.password,
+            "telePhoneNumber": this.telePhoneNumber,
+            "classInformation": this.classInformation,
+            "loginStatus": this.signInStatus
+        };
+    },
+    setStatus: function() {
+        this.signInStatus = true;
+    },
+    backInformation: function() {
+
+    }
+}
+
 var Post = new postChoose(document.getElementById("leftAngle"), document.getElementById("rightAngle"));
 Post.whenCreated();
+var submitInformation = new Submit();
+submitInformation.alive();
