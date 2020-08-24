ed567fcec290452b23e34047e968f122d0a777c1
lovebaihezi <x18739168862@gmail.com>
2020-08-24 11:16:07 +0800

    Restart

--- FEintroduction-RH/JS.js
+++
@@ -1,6 +0,0 @@
-var index=0;
-    var word=document.getElementById("w").innerHTML;
-    function type(){
-        document.getElementById("aa").innerText = word.substring(0,index++);
-    }
-    setInterval(type, 200);
