839c112780ba1335c790c8a3db40da894d98708a
lovebaihezi <x18739168862@gmail.com>
2020-09-03 00:27:34 +0800

    Plan1

--- JS/ToolBox.js
+++ JS/ToolBox.js
@@ -1,6 +1,6 @@
-var upToTop = document.getElementById("BackToTop");
+const upToTop = document.getElementById("BackToTop");
 window.onscroll = function() {
-    ScrollHeight = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop);
+    let ScrollHeight = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop);
     if (ScrollHeight >= 500) {
         upToTop.style.visibility = "visible";
     }
@@ -15,11 +15,20 @@ function ToolBox(ElementA, ElementB) {
 }
 
 ToolBox.prototype.addAction = function(Action, TargetAction) {
-    var This = this;
+    const This = this;
     this.Subject.addEventListener(Action, function() { TargetAction(This.ActionObject) });
 }
 
 const wantToLogin = new ToolBox(document.getElementById("logIn"), document.getElementById("logInAndApply"));
 const closeLoginAndApply = new ToolBox(document.getElementById("closeLoginAndApply"), document.getElementById("logInAndApply"));
+const showPad = new ToolBox(document.getElementById("PadBox"), document.getElementById("PadBox"));
+const shrinkPad = new ToolBox(document.getElementById("closePad"), document.getElementById("PadBox"));
+
+
 wantToLogin.addAction("click", function(A) { A.classList.remove("close"); });
-closeLoginAndApply.addAction("click", function(A) { A.classList.add("close") });
+closeLoginAndApply.addAction("click", function(A) { A.classList.add("close"); });
+showPad.addAction("click", function(A) { A.classList.remove("childAbsolute"); });
+shrinkPad.addAction("click", function(A) {
+    A.classList.add("childAbsolute");
+    console.log(A.classList);
+});
