c20e5a180d720a7115c851d86e2ba98c2b191800
lovebaihezi <x18739168862@gmail.com>
2020-09-16 17:50:33 +0800

    add su.html add some features for style

--- JS/Login.js
+++ JS/Login.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.onload = () => {
     let loginButton = document.getElementById("Submit");
     let formInformation = document.getElementsByTagName("form")[0];
     let inputList = document.getElementsByTagName("input");
@@ -7,70 +7,22 @@ window.onload = function() {
     let AjaxObject;
     let ResponseTextFromServer;
     let ResponseInformationObject;
-    formInformation.addEventListener("submit", function(form) {
-        form.preventDefault();
+    formInformation.addEventListener("submit", (formEvent) => {
+        formEvent.preventDefault();
         for (i = 0; i < formInformation.elements.length - 1; i++) {
-            (function(i) {
+            ((i) => {
                 JsonDataObject[formInformation.elements[i].name] = formInformation.elements[i].value;
             })(i);
         }
-        JsonData = JSON.stringify(JsonDataObject);
-        try {
-            AjaxObject = new XMLHttpRequest();
-        } catch (err) {
-            AjaxObject = new ActiveXObject("Microsoft.XMLHTTP ");
-            alert("Your explorer is too old to use,try firefox please");
-        }
+        AjaxObject = new XMLHttpRequest();
         window[form.submitter.value]();
     }, false);
 
-    window.LOGIN = function() {
-        AjaxObject.open("post", "https://lqxclqxc.com/Test.php", true);
-        // AjaxObject.setRequestHeader("Content-Type", "application/json");
-        AjaxObject.onreadystatechange = function() {
-            if (AjaxObject.readyState == 4 && AjaxObject.status == 200) {
-                ResponseTextFromServer = AjaxObject.responseText;
-                ResponseInformationObject = JSON.parse(ResponseTextFromServer);
-                ResponseInformationObject.status = 2;
-                // 0 1 2
-                // 登陆失败 登录但未注册 登录而且已经注册
-                switch (ResponseInformationObject.status) {
-                    case 0:
-                        {
-                            for (i = 0; i < formInformation.elements.length - 1; i++) {
-                                (function(i) {
-                                    formInformation.elements[i].value = null;
-                                })(i);
-                            }
-                            location.reload();
-                            break;
-                        }
-                    case 1:
-                        {
-                            location.replace("./Apply.html");
-                            break
-                        }
-                    case 2:
-                        {
-                            AjaxObject.open("get", "./InformationSearch.html?i=" + ResponseTextFromServer, true);
-                            AjaxObject.onreadystatechange = function() {
-                                if (AjaxObject.readyState == 4 && AjaxObject.status == 200) {
-                                    // if (AjaxObject.responseText == 1) {
-                                    location.replace("./InformationSearch.html?i=" + ResponseTextFromServer);
-                                    // }
-                                }
-                            }
-                            AjaxObject.send();
-                            break;
-                        }
-                }
-            }
-        }
-        AjaxObject.send(JsonData);
+    window.LOGIN = () => {
+
     }
 
-    window.APPLY = function() {
-        location.replace("./InformationSearch.html");
-    }
+    window.APPLY = () => {
 
+    }
 }
