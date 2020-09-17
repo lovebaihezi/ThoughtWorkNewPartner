7cff122df159990433ddd419984e0b606cd9ae9b
lovebaihezi <x18739168862@gmail.com>
2020-09-17 20:09:27 +0800

    add features to manage ,show,confirm, comment ,and etc

--- JS/Login.js
+++ JS/Login.js
@@ -1,13 +1,30 @@
 window.onload = () => {
     let loginButton = document.getElementById("Submit");
-    let formInformation = document.getElementsByTagName("form")[0];
+    let leftButton = document.getElementById("leftButton");
+    let rightButton = document.getElementById("rightButton");
+    formInformation = document.getElementsByTagName("form")[0];
+    leftButton.addEventListener("click", () => {
+        leftButton.classList.remove("on");
+        rightButton.classList.add("on");
+        formInformation = document.getElementsByTagName("form")[0];
+        formInformation.classList.remove("close");
+        document.getElementsByTagName("form")[1].classList.add("close");
+    });
+
+    rightButton.addEventListener("click", () => {
+        leftButton.classList.add("on");
+        rightButton.classList.remove("on");
+        formInformation = document.getElementsByTagName("form")[1];
+        formInformation.classList.remove("close");
+        document.getElementsByTagName("form")[0].classList.add("close");
+    });
     let inputList = document.getElementsByTagName("input");
     let JsonDataObject = {};
     let JsonData;
     let AjaxObject;
     let ResponseTextFromServer;
     let ResponseInformationObject;
-    formInformation.addEventListener("submit", (formEvent) => {
+    let NewDefault = (formEvent) => {
         formEvent.preventDefault();
         for (i = 0; i < formInformation.elements.length - 1; i++) {
             ((i) => {
@@ -15,8 +32,11 @@ window.onload = () => {
             })(i);
         }
         AjaxObject = new XMLHttpRequest();
-        window[form.submitter.value]();
-    }, false);
+        window[formEvent.submitter.value]();
+    }
+    document.getElementsByTagName("form")[0].addEventListener("submit", NewDefault(form), true);
+
+    document.getElementsByTagName("form")[1].addEventListener("submit", NewDefault(form), true);
 
     window.LOGIN = () => {
             JsonDataObject.Password = encryptDate(JsonDataObject.Password);
@@ -26,8 +46,18 @@ window.onload = () => {
     window.APPLY = () => {
 
     }
+
+    window.IAmADMIN = () => {
+
+    }
 }
 
 function encryptDate(StringObject) {
+    let RsaAjax = new XMLHttpRequest();
+    let rsa = new RSAKey();
+    RsaAjax.open("http://www.zfjw.xupt.edu.cn/jwglxt/xtgl/login_getPublicKey.html?time=" + new Date().getTime(), "post", false);
+    RsaAjax.addEventListener("readystatechange", () => {
 
+    });
+    RsaAjax.send();
 }
