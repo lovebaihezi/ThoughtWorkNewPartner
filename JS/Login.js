55b7b7e0cf7f5f876409d7cda3fa5b0f2da12619
lovebaihezi <x18739168862@gmail.com>
2020-09-17 21:47:59 +0800

    second try

--- JS/Login.js
+++ JS/Login.js
@@ -64,16 +64,20 @@ window.onload = () => {
 }
 
 function encryptDate() {
-    let RsaAjax = new XMLHttpRequest();
-    // http://www.zfjw.xupt.edu.cn/jwglxt/xtgl/login_getPublicKey.html
-    let rsa = new RSAKey();
-    RsaAjax.open("get", "http://www.zfjw.xupt.edu.cn/jwglxt/xtgl/login_getPublicKey.html?time=" + new Date().getTime(), false);
-    RsaAjax.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36");
-    RsaAjax.addEventListener("readystatechange", () => {
-        if (RsaAjax.readyState == 4 && RsaAjax.status == 200) {
-            console.log(RsaAjax.response);
-        }
+    // let RsaAjax = new XMLHttpRequest();
+    // // http://www.zfjw.xupt.edu.cn/jwglxt/xtgl/login_getPublicKey.html
+    // let rsa = new RSAKey();
+    // RsaAjax.open("get", "http://www.zfjw.xupt.edu.cn/jwglxt/xtgl/login_getPublicKey.html?time=" + new Date().getTime(), false);
+    // RsaAjax.setRequestHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36");
+    // RsaAjax.addEventListener("readystatechange", () => {
+    //     if (RsaAjax.readyState == 4 && RsaAjax.status == 200) {
+    //         console.log(RsaAjax.response);
+    //     }
+    // });
+    // RsaAjax.send();
+    $.getJSON("http://www.zfjw.xupt.edu.cn/jwglxt" + "/xtgl/login_getPublicKey.html?time=" + new Date().getTime(), function(data) {
+        modulus = data["modulus"];
+        exponent = data["exponent"];
     });
-    RsaAjax.send();
 }
 // "User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0"
