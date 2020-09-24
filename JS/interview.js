c8a6cd3b7237b302b3268cf31b3da7e657540365
lovebaihezi <x18739168862@gmail.com>
2020-09-24 22:46:15 +0800

    fix bug

--- JS/interview.js
+++ JS/interview.js
@@ -1,3 +1,7 @@
 window.onload = () => {
+    let apply = document.getElementById("apply");
+    apply.onclick = function() {
+
+    }
     let comment = document.getElementById("comment").innerHTML;
 }
