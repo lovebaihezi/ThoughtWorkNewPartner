bd68527f0271ab857048c2003188c526fe7fbab5
lovebaihezi <x18739168862@gmail.com>
2020-09-24 16:45:02 +0800

    change?

--- JS/interview.js
+++ JS/interview.js
@@ -1,3 +1,4 @@
 window.onload = () => {
-    
+    let commment = document.getElementById("comment").innerHTML;
+    console.log(document.cookie);
 }
--- JS/su.js
+++ JS/su.js
@@ -1,1 +1,23 @@
 // Copyright (c) chai bo wen LQXC All rights reserved.
+window.onload = () => {
+    let interview = document.getElementById("");
+    interview.isClick = false;
+    interview.addEventListener("click",
+        ()=>{
+            if(interview.isClick)
+            {
+                return;
+            }
+            interview.isClick=true;
+            let ID = document.getElementById("studentID").value;
+            let sendID = new xmlHTTPRequest();
+            sendID.open("post","http://localhost:3000/studentInterview",true);
+            sendID.onreadystatechange = () = {
+                if(sendID.status == 200 && sendID.readyState == 4){
+                    document.cookie.student =JSON.parse(sendID.responseText);
+                    alert("find it!");
+                }
+            }
+            sendID.send(ID);
+        });
+}
