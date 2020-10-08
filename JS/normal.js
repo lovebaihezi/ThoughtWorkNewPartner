c7840448bf76cdc0b4bac7cde2f9c4dccc31c977
lovebaihezi <x18739168862@gmail.com>
2020-10-08 22:38:25 +0800

    fix bug

--- JS/normal.js
+++ JS/normal.js
@@ -1,14 +1,12 @@
-window.onload = function() {
-    let back = document.getElementById("back");
-    back.onclick = function() {
-        history.go(-1);
-    }
-    let loginButton = document.getElementsByClassName("login")[0];
-    if (sessionStorage.student) {
-        location.replace("../HTML/informationSearch.html");
-    } else {
-        loginButton.onclick = function() {
-            location.href = "../HTML/Login.html";
-        }
+let back = document.getElementById("backButton");
+back.onclick = function() {
+    location.replace("../HTML/selection.html")
+}
+let loginButton = document.getElementsByClassName("loginButton")[0];
+if (sessionStorage.student) {
+    location.replace("../HTML/informationSearch.html");
+} else {
+    loginButton.onclick = function() {
+        location.href = "../HTML/Login.html";
     }
 }
